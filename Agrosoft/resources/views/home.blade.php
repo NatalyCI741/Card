300d491cb15cd01e5581609c721a17fab8fd8e29
Natty <chalaipuznataly@gmail.com>
2025-08-13 14:35:01 -0500

    Delete Agrosoft directory

--- Agrosoft/resources/views/home.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.master')
-
-@section('content')
-<center><img src="{{ asset('images/LogoAgrosoft2.png') }}" width="360php"></center>
-@endsection
--- Agrosoft/resources/views/layouts/unidades/listac.blade.php
+++
@@ -1,3 +0,0 @@
-@extends('layouts.plantilla')
-@section('titulopagina', 'Lista de Cultivos Rentables')
-@section('contenido')
